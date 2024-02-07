9668cc161bdc345349f9f733147b9c81fbe792ba
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-07 15:00:08 +0000

    remove scores from Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,7 @@
-import styles from "../styles/index.module.css";
-
-export default function Header({ currentScore, highestScore }) {
+export default function Header({}) {
   return (
     <header>
       <h1>PokéMemory</h1>
-      <div className={styles.scoreboard}>
-        <p className={styles.currentScore}>Current Score: {currentScore}</p>
-        <p className={styles.highestScore}>Highest Score: {highestScore}</p>
-      </div>
     </header>
   );
 }
